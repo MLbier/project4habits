b5bec1853895e20eab5a91d4376e1dc3e618b30f
Paul Henri Eichhorn <paul.henri.eichhorn@gmail.com>
2021-11-29 15:04:18 +0100

    Booking only displays unbooked slots

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -4,13 +4,18 @@ class BookingsController < ApplicationController
     @booking = Booking.new
   end
 
+  #if there is a booking for kid_id with the slot_id then the kid cannot book again
+
   def create
     @booking = Booking.new
     @kid = Kid.find(params[:kid_id])
     @slot = Slot.find(params[:slot_id])
-    @booking.slot = @slot
-    @booking.kid = @kid
-    @booking.save
+    @already_booked = Booking.where(slot: @slot, kid: @kid).exists?
+    unless @already_booked
+      @booking.slot = @slot
+      @booking.kid = @kid
+      @booking.save
+    end
     redirect_to kid_path(@kid)
   end
 
