0021f1708113a0a11e41130a3e19b9e521655151
Marie-Luise Hahn <marieluisehahn@ymail.com>
2021-12-01 11:56:08 +0100

    trilio

--- app/controllers/allerts_controller.rb
+++ app/controllers/allerts_controller.rb
@@ -1,5 +1,16 @@
 class AllertsController < ApplicationController
-  def new
-    @allert = Allert.new
+  def notify
+    @booking_id = params[:id]
+
+    client = Twilio::REST::Client.new(ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN'])
+    client.messages.create({
+    from: '+12077076871',
+    to: '+4524777766',
+    body: 'Hello there! This is a test'
+    })
+
+    redirect_to slot_bookings_path(slot_id: @booking_id)
   end
+
+
 end
