20e52f8853d850f1b9429be6079a1fce0af220f7
Andrii Gorokhovskyi <andrii.gorokhovskyi@gmail.com>
2021-12-03 13:43:32 +0200

    checklist

--- .history/app/controllers/application_controller_20211203125452.rb
+++ .history/app/controllers/application_controller_20211203125452.rb
@@ -10,6 +10,6 @@ class ApplicationController < ActionController::Base
   end
 
   def default_url_options
-  { host: ENV["DOMAIN"] || "www.4habits.fun" }
+  { host: ENV["DOMAIN"] || "localhost:3000" }
   end
 end
